f706395582439106bdc87b4de3e7c807e07d06e2
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-08 15:16:17 -0500

    setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+server.use(express.json());
 // require your posts router and connect it here
+
+const postRouter = require('./posts/posts-router');
+server.use('/api/post', postRouter);
+
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message: 'page not found'
+    });
+})
+
+module.exports = server;
