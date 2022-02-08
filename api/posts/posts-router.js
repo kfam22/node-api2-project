b7e5137cb06daab46fdbe967ddd50f7629540be0
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-08 15:37:10 -0500

    getting posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -3,7 +3,16 @@ const Posts = require('./posts-model');
 const router = require('express').Router();
 
 router.get('/', (req, res) =>{
-    res.json('getting posts');
+    Posts.find()
+    .then(posts =>{
+        res.status(200).json(posts)
+    })
+    .catch(err =>{
+        res.status(500).json({
+            message: 'The posts information could not be retrieved',
+            error: err.message
+        })
+    })
 })
 router.get('/:id', (req, res) =>{
 
@@ -20,10 +29,6 @@ router.delete('/:id', (req, res) =>{
 router.get('/:id/comments', (req, res) =>{
 
 })
-
-
-
-
 
 module.exports = router;
 
